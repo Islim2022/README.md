b4e66dec09da218a1650ee1091071505ae29e36c
Islim2022 <100063345+Islim2022@users.noreply.github.com>
2022-03-15 17:45:11 +0200

    Delete fizzBuzz.js

--- fizzBuzz.js
+++
@@ -1,6 +0,0 @@
-for (var i = 1; i < 101; i++) {
-    if (i % 15 == 0) console.log("FizzBuzz");
-    if (i % 3 == 0) console.log("Fizz");
-    if (i % 5 == 0) console.log("Buzz");
-    else console.log(i);
-}
